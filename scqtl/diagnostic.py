98acb6174a741a66525b93896195854a68d65c8b
Abhishek Sarkar <aksarkar@alum.mit.edu>
2018-12-09 22:30:36 -0600

    Fix the diagnostic test

--- scqtl/diagnostic.py
+++ scqtl/diagnostic.py
@@ -3,6 +3,7 @@ import scipy.special as sp
 import scipy.stats as st
 
 def rpp(x, log_mu, log_phi, logodds, size, onehot, n_samples=100):
+  # Important: these are n x 1
   n = onehot.dot(np.exp(-log_phi))
   pi0 = onehot.dot(sp.expit(-logodds))
   p = 1 / (1 + (size * onehot.dot(np.exp(log_mu + log_phi))))
@@ -14,7 +15,9 @@ def rpp(x, log_mu, log_phi, logodds, size, onehot, n_samples=100):
   pmf = st.nbinom(n=n, p=p).pmf(x)
   pmf *= (1 - pi0)
   pmf[x == 0] += pi0[x == 0]
-  rpp = cdf + np.random.uniform(size=(n_samples, x.shape[0])) * pmf
+  u = np.random.uniform(size=(n_samples, x.shape[0]))
+  # cdf and pmf are n x 1
+  rpp = cdf.ravel() + u * pmf.ravel()
   return rpp
 
 def diagnostic_test(x, log_mu, log_phi, logodds, size, onehot, n_samples=100):
